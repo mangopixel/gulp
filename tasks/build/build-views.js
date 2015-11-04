1aec35b665c588161fd7519be948aa3c00e43a88
Flugger <flugged@gmail.com>
2015-11-04 13:42:20 +0100

    Build views

--- tasks/build/build-views.js
+++ tasks/build/build-views.js
@@ -2,7 +2,7 @@
  *
  */
 module.exports = function ( gulp, options, plugins ) {
-    gulp.task( 'build:copy-views', function() {
+    gulp.task( 'build:views', function() {
 
         return gulp.src( options.config.source + '/views/**/*.html' )
             .pipe( gulp.dest( options.config.build + '/views' ) );
