c7d303faac04ca6b24208f5ed683fa369bf5b522
Flugger <flugged@gmail.com>
2015-12-05 17:01:52 +0100

    Ignore favicon on move assets

--- tasks/build/build-assets.js
+++ tasks/build/build-assets.js
@@ -4,7 +4,7 @@
 module.exports = function ( gulp, options, plugins ) {
     gulp.task( 'build:assets', function() {
 
-        return gulp.src( options.config.source + '/assets/**/*.*' )
+        return gulp.src( options.config.source + '/assets/**/*.*', '!' + options.config.source + '/assets/favicon.png' )
             .pipe( gulp.dest( options.config.build ) );
     } );
 };
